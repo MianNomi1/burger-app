da38e6a151a66ebf87000aee49e31f8b49610b7b
Mian Nouman Rahsid-External <nouman.rashid@systemsltd.com>
2019-10-07 06:47:29 -0700

    Burger Component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,2 +1,17 @@
 import React from "react";
+import BurgerIngredient from "../Burger/BurgerIngredients/BurgerIngrediants";
+const burger = () => {
+    return (
+        <div className="Burger">
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="bacon" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="salad" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
 
+    );
+};
+
+export default burger;
