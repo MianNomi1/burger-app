8a113921c434852d4753c2ae1af55f5faceaf441
Mian Nouman Rahsid-External <nouman.rashid@systemsltd.com>
2019-10-13 23:47:42 -0700

    Built Checkout Order Summary Componet

---
+++ src/components/Orders/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Buttons/Button";
+const checkoutSummary = (props) => {
+    return (<div className="CheckoutSummary">
+        <h1>We hope it tastes Well !</h1>
+        <div style={{ width: '300px', margin: 'auto' }}>
+            <Burger ingredients={props.ingredients} />
+        </div>
+        <Button
+            btnType='Danger'
+            clicked>CANCEL</Button>
+        <Button
+            btnType='Success'
+            clicked>CONTINUE</Button>
+    </div>);
+}
+
+export default checkoutSummary;
